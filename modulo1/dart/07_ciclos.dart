8e63184b1697200dd4b4ff4cfa650118ade0a353
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2025-10-15 19:56:24 -0500

    Miercoles 15 de octubre del 2025

---
+++ modulo1/dart/07_ciclos.dart
@@ -0,0 +1,21 @@
+void main(){
+    int n = 10;
+    double suma =0;
+    for (int i=1;i<=n;i++){
+      suma+=i;
+    }
+  
+  print("La suma de 1... $n es $suma");
+}
+
+
+
+void main(){
+    int n = 10;
+    double multiplicar = 0
+
+    for (int i=1;i<=n1++){
+        multiplicar=multiplicar*i
+    }
+    print(multiplicar)
+}
